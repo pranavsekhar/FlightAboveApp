93668d90df4418a6b032a945d4e60db77a21f548
pranavsekhar <pranavsekhar25@gmail.com>
2025-11-07 18:49:39 -0800

    Initial Commit

---
+++ FlightAboveApp/FlightAboveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightAboveAppApp.swift
+//  FlightAboveApp
+//
+//  Created by Pranav Sekhar on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FlightAboveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
